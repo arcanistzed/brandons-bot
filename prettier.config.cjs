2a37121e803a1127bb3824ed66a5e49570c1c36a
Daniel Thorp <arcanistzed@gmail.com>
2023-06-25 18:56:24 -0400

    feat: initial commit
    Signed-off-by: Daniel Thorp <arcanistzed@gmail.com>

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+	plugins: [],
+	printWidth: 80,
+	tabWidth: 4,
+	semi: true,
+	useTabs: true,
+	singleQuote: false,
+	trailingComma: "all",
+	bracketSpacing: true,
+	arrowParens: "avoid",
+	proseWrap: "preserve",
+};
